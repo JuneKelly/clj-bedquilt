c5e8270a2ed7c5fb6345363c0404db167f5cec17
Shane Kilkelly <shane@kilkelly.me>
2014-05-12 15:06:00 +0100

    moving things around

--- src/bedquilt/core.clj
+++ src/bedquilt/core.clj
@@ -1,5 +1,5 @@
 (ns bedquilt.core
-  (:require [clojure.java.jdbc :as sql]))
+  (:require [clojure.java.jdbc :as jdbc]))
 
 
 (defn get-db [{:keys [db-host db-name user password]}]
@@ -14,11 +14,11 @@
 
 
 (defn collection-exists? [db-spec collection-name]
-  (let [result (sql/query db-spec
-                          [(str "SELECT EXISTS(
-                                 SELECT * FROM information_schema.tables
-                                 WHERE table_schema = 'public' AND
-                                 table_name = '" collection-name "');")])
+  (let [result (jdbc/query db-spec
+                           ["SELECT EXISTS(
+                             SELECT * FROM information_schema.tables
+                             WHERE table_schema = 'public' AND
+                             table_name = ?);" collection-name])
         exists (= true (:exists (first result)))]
     exists))
 
@@ -26,12 +26,35 @@
 (defn create-collection! [db-spec collection-name]
   (if (not (collection-exists? db-spec collection-name))
     (do
-      (sql/db-do-commands db-spec
-                          (sql/create-table-ddl collection-name
-                                                [:_id "varchar(32)"]
-                                                [:data "json"]))
+      (jdbc/db-do-commands
+       db-spec
+       (jdbc/create-table-ddl collection-name
+                             [:_id "uuid"]
+                             [:data "json"]))
       true)
     false))
 
 
+(defn has-id? [m]
+  (contains? m :_id))
 
+
+(defn map->row
+  "convert map data into a list of items
+   suitable for inserting into table,
+   generating _id field if necessary"
+  [m]
+  (if (has-id? m)
+    {:id (:_id m)
+     :data (dissoc :_id m)}
+    {:id (generate-id!)
+     :data m}))
+
+
+(defn row->map [row-data]
+  (comment "TODO"))
+
+
+(defn insert! [data]
+  (let [row-data (map->row data)]
+    nil))
