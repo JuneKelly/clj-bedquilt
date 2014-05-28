222833615cb5a7c8c720e613169d8f0f6d42cfaa
Shane Kilkelly <shane@kilkelly.me>
2014-05-28 18:55:24 +0100

    update version, and license, make the project.clj look a bit better

--- project.clj
+++ project.clj
@@ -1,8 +1,12 @@
-(defproject bedquilt "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject bedquilt "0.2.0"
+
+  :description "A JSON document store on PostgreSQL"
+
+  :url "https://github.com/ShaneKilkelly/bedquilt"
+
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/java.jdbc "0.3.3"]
                  [cheshire "5.3.1"]
@@ -11,5 +15,7 @@
                  [postgresql/postgresql "8.4-702.jdbc4"]]
 
   :profiles {:dev {:dependencies [[speclj "2.5.0"]]}}
+
   :plugins [[speclj "2.5.0"]]
+
   :test-paths ["spec"])
